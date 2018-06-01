11e5de2eae841fd18de77b78bf1a50122f0e8473
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-06-01 14:31:08 +0300

    Added demo project

---
+++ Hammer/HammerDemo/HammerDemo/ViewControllers/Main/MainViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MainViewController.swift
+//  HammerDemo
+//
+//  Created by Igor Matyushkin on 01.06.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Outlets
+    
+    // MARK: Object variables & properties
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize navigation bar
+        
+        self.navigationItem.title = "Main"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Private object methods
+    
+    // MARK: Actions
+    
+    // MARK: Protocol implementation
+    
+}
